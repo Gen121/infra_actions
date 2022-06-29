dab25058ba6242fbe010eca1309ce4accbfb0407
Eugene Che <grem1in.che@yandex.ru>
2022-06-29 15:05:28 +0300

    Plugged in django-environ

--- infra_project/infra_project/settings.py
+++ infra_project/infra_project/settings.py
@@ -12,20 +12,25 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
+import environ
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'k9gwcfq01xszx2(_4e#gc3u$rt=)y6ivx=$wt+5&-^ktk4dg4e'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']  # may be it should be in .env file?
+ALLOWED_HOSTS = [env('ALLOWED_HOSTS'), ]  # may be it should be in .env file?
 
 
 # Application definition
